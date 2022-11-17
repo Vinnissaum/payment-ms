9ec2be1384bd3ee82603bbe3328ff7eef90121dc
Vinicius Braz <vinicius_lsb@live.com>
2022-11-17 10:34:08 -0300

    feat: add custom exception to handle with database exceptions

---
+++ src/main/java/br/com/vinissaum/payment/services/exceptions/DatabaseException.java
@@ -0,0 +1,7 @@
+package br.com.vinissaum.payment.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+    public DatabaseException(String message) {
+        super(message);
+    }
+}
