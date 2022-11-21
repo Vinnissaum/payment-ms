4edd5f4928c9bc580e72f97c5afe5a7e80da4c12
Vinicius Braz <vinicius_lsb@live.com>
2022-11-20 22:15:47 -0300

    feat: add client interface to communicate using feign client

---
+++ src/main/java/br/com/vinissaum/payment/http/OrderClient.java
@@ -0,0 +1,14 @@
+package br.com.vinissaum.payment.http;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient("orders-ms")
+public interface OrderClient {
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/orders/{id}/paid")
+    void updatePayment(@PathVariable Long id);
+
+}
