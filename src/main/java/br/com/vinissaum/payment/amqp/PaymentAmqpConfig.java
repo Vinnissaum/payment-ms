8fd37cb922465d135dbc459622fbcf0ba90bd6c4
Vinicius Braz <vinicius_lsb@live.com>
2022-11-23 14:43:56 -0300

    feat: add rabbitmq config

---
+++ src/main/java/br/com/vinissaum/payment/amqp/PaymentAmqpConfig.java
@@ -0,0 +1,30 @@
+package br.com.vinissaum.payment.amqp;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PaymentAmqpConfig {
+
+    @Bean
+    public Queue createQueue() {
+        return QueueBuilder.nonDurable("payment.successful").build();
+    }
+
+    @Bean
+    public RabbitAdmin createRabbitAdmin(ConnectionFactory conn) {
+        return new RabbitAdmin(conn);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> initAdmin(RabbitAdmin rabbitAdmin){
+        return event -> rabbitAdmin.initialize();
+    }
+
+}
